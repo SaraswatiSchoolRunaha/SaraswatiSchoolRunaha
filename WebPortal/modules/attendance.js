f0e9a57377b47e9cffafc7f552f5e7e2330e0244
SaraswatiSchoolRunaha <saraswatischoolrunaha2023@gmail.com>
2026-06-13 21:27:32 +0530

    Update attendance.js

--- WebPortal/modules/attendance.js
+++ WebPortal/modules/attendance.js
@@ -235,16 +235,18 @@ function saveAttendanceToSheets(filteredStudents) {
     btn.innerText = "⏳ डेटा भेजा जा रहा है...";
 
     fetch(sheetUrls['Attendance'], {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            action: "saveAttendance",
-            data: attendanceData
-        })
+    method: "POST",
+    mode: "no-cors",
+    headers: {
+        "Content-Type": "text/plain;charset=utf-8"
+    },
+    body: JSON.stringify({
+        action: "saveAttendance",
+        data: attendanceData
     })
-    .then(async (res) => {
+});
+    
+.then(async (res) => {
         const text = await res.text();
         console.log("Server Response:", text);
 
