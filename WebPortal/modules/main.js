6b6f14e60979e2fd4f189d8980fc2eb84b6b4b24
SaraswatiSchoolRunaha <saraswatischoolrunaha2023@gmail.com>
2026-06-14 22:41:55 +0530

    Update main.js

--- WebPortal/modules/main.js
+++ WebPortal/modules/main.js
@@ -86,7 +86,7 @@ function executeModuleRouting(title) {
         case 'उपस्थित छात्रों की सूची':
             showPresentReport();
             break;
-            case 'शिक्षक उपस्थिति': 
+        case 'शिक्षक उपस्थिति': 
             loadTeacherAttendance();
             break;
         default:
