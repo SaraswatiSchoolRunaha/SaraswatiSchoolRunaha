5aad3ce47dbae3972e6ef35f678259348fee5ffa
SaraswatiSchoolRunaha <saraswatischoolrunaha2023@gmail.com>
2026-06-15 21:23:32 +0530

    Update teacherAttendance.js

--- WebPortal/modules/teacherAttendance.js
+++ WebPortal/modules/teacherAttendance.js
@@ -1,5 +1,4 @@
 import { sheetUrls, translations, state } from './config.js';
-import { loadTeacherAttendance } from './teacherAttendance.js';
 
 export function loadTeacherAttendance() {
     // 1. एचटीएमएल (UI) को रेंडर करना जहाँ अटेंडेंस का कार्ड दिखेगा
