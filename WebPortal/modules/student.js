df49833be5b54e205adab9995cc290a7a0ef4a4f
SaraswatiSchoolRunaha <saraswatischoolrunaha2023@gmail.com>
2026-06-25 21:00:56 +0530

    Update student.js

--- WebPortal/modules/student.js
+++ WebPortal/modules/student.js
@@ -8,16 +8,19 @@ export async function getStudentsByFilter(className, medium) {
 }
 
 export async function promoteSelectedStudent(studentIds, targetClass, targetSession) {
+
+    const formData = new URLSearchParams();
+
+    formData.append("action", "bulkPromote");
+    formData.append("ids", JSON.stringify(studentIds));
+    formData.append("newClass", targetClass);
+    formData.append("newSession", targetSession);
+
     const response = await fetch(sheetUrls['Database'], {
-        method: 'POST',
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ 
-            action: 'bulkPromote', 
-            ids: studentIds,
-            newClass: targetClass,
-            newSession: targetSession 
-        })
+        method: "POST",
+        body: formData
     });
+
     return await response.json();
 }
 
