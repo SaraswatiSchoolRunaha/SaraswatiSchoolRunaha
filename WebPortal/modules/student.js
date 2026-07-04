776e29f5095cbe60a149b4eb916ca7c5595cd4ee
SaraswatiSchoolRunaha <saraswatischoolrunaha2023@gmail.com>
2026-07-04 19:52:45 +0530

    Update student.js

--- WebPortal/modules/student.js
+++ WebPortal/modules/student.js
@@ -414,21 +414,8 @@ function getProfileFormHTML(data) {
             <div class="field"><label>Enrolment No</label><input id="uEnrol" class="portal-input" value="${data.enrolment || ''}"></div>
             <div class="field"><label>Mobile</label><input id="uMobile" class="portal-input" value="${data.mobile1 || ''}"></div>
             <div class="field" id="subField" style="display:${(data.class=='XI'||data.class=='XII')?'flex':'none'}"><label>Subject</label><input id="uSubject" class="portal-input" value="${data.subject || ''}"></div>
-            <div class="field">
-                <label>Type of Student</label>
-                <select id="uStudentType" class="portal-input">
-                <option value="Regular" ${data.studentType=='Regular'?'selected':''}>New</option>
-                <option value="Private" ${data.studentType=='Private'?'selected':''}>Old</option>
-                </select>
-                </div>
             <div class="field" style="grid-column: span 2;"><label>Address</label><input id="uAddress" class="portal-input" value="${data.address || ''}"></div>
-                <div class="field">
-                <label>Type of Student</label>
-                <select id="uStudentType" class="portal-input">
-                <option value="Regular" ${data.studentType=='Regular'?'selected':''}>New</option>
-                <option value="Private" ${data.studentType=='Private'?'selected':''}>Old</option>
-                </select>
-                </div>
+                
                 
             <div class="section-title">🏦 बैंक एवं सुरक्षा विवरण (Bank & Security)</div>
             <div class="field"><label>Aadhaar</label><input class="portal-input input-disabled" value="[Redacted]" disabled></div>
